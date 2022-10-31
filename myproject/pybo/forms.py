b030f530c38a02c399661f51a2091fd09c7dafe3
namicad <namicad@gmail.com>
2022-11-01 00:37:48 +0900

    s

--- myproject/pybo/forms.py
+++ myproject/pybo/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class QuestionForm(FlaskForm):
     subject = StringField('title', validators=[DataRequired('require title')])
@@ -8,3 +8,11 @@ class QuestionForm(FlaskForm):
 
 class AnswerForm(FlaskForm):
     content = TextAreaField('content', validators=[DataRequired('require content')])
+
+class UserCreateForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(min=3, max=25)])
+    password1 = PasswordField('password', validators=[DataRequired(), EqualTo('password2', 'Password does not match.')])
+    password2 = PasswordField('check password', validators=[DataRequired()])
+
+class LoginForm(FlaskForm):
+    id = StringField('id', validators=[DataRequired(), Length(min=3, max=25)])
